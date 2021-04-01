bd49e9159c5eb7352039e555fcfad3935065fd30
Jayleen Li <jayleenli17@gmail.com>
2021-04-01 00:28:07 -0700

    remove unusued imports

--- app/src/main/java/com/example/womxnhacksandroidworkshop/MainActivity.java
+++ app/src/main/java/com/example/womxnhacksandroidworkshop/MainActivity.java
@@ -3,13 +3,8 @@ package com.example.womxnhacksandroidworkshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
